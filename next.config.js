a32bdb0da2d9e6e84be00f7c548c1c2840b95642
Alexei <a.krekotun@gmail.com>
2019-10-20 11:15:09 +0300

    remove typescript + remove Container component

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
-const withTypescript = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
 
-module.exports = withTypescript(
-  withSass({
-    webpack(config, options) {
-      return config
-    },
-    cssModules: true,
-    sassLoaderOptions: {
-      data: `
+module.exports = withSass({
+  webpack(config, options) {
+    return config
+  },
+  cssModules: true,
+  sassLoaderOptions: {
+    data: `
         @import 'src/views/styles/mixins/index.scss';
       `,
-    },
-  }),
-)
+  },
+})
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import React from 'react'
 import { initializeStore } from 'src/store'
 import { Provider } from 'mobx-react'
@@ -42,11 +42,9 @@ class MyMobxApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
-      <Container>
-        <Provider {...this.mobxStore} {...this.application}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
+      <Provider {...this.mobxStore} {...this.application}>
+        <Component {...pageProps} />
+      </Provider>
     )
   }
 }
